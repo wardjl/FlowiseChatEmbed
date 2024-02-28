99b105bc8fa1821e73995d2fda2fbe009519426d
Ward <wardleenders@gmail.com>
2024-02-28 20:21:48 +0100

    Remove Branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -44,17 +44,16 @@ export const Badge = (props: Props) => {
         'background-color': props.badgeBackgroundColor ?? '#ffffff',
       }}
     >
-      Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={''}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span></span>
       </a>
     </span>
   );
